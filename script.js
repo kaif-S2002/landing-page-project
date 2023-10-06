b61a28da926f4048b76328b4d44d14a10f816383
kaif siddiqui <kaifs8998@gmail.com>
2023-10-07 01:01:47 +0530

    feat- make navbar responsive

--- script.js
+++ script.js
@@ -1,5 +1,21 @@
 const nav = document.querySelector(".nav");
+const menuBtn = document.querySelector(".menu-btn");
+const open = document.getElementById("hamb");
+const close = document.getElementById("cross");
+const menu = document.querySelector(".list");
 
 window.addEventListener("scroll", () => {
   nav.classList.toggle("sticky", window.scrollY > 24);
+});
+
+menuBtn.addEventListener("click", () => {
+  if (open.style.display == "flex") {
+    open.style.display = "none";
+    close.style.display = "flex";
+    menu.style.display = "flex";
+  } else {
+    open.style.display = "flex";
+    close.style.display = "none";
+    menu.style.display = "none";
+  }
 });
